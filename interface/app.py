017c418ef4b62ce8e3fae41c58dd6fe1e37ba92a
yago <100451322@alumnos.uc3m.es>
2023-11-27 18:17:31 +0100

    Cambios en las fuentes de datos

--- interface/app.py
+++ interface/app.py
@@ -15,31 +15,38 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import numpy as np
 
+# Creamos la aplicación de flask
 app = Flask(__name__, template_folder='.', static_folder='.', static_url_path='')
+
+# Cargar el modelo
 modelo = load_model('../modelo/modelo.hdf5')
+# Copiar las clases del cuadernillo
+clases = {0: 'Bulbasaur', 1: 'Caterpie', 2: 'Charizard', 3: 'Charmander',
+          4: 'Dragonite', 5: 'Eevee', 6: 'Gengar', 7: 'Geodude', 8: 'Greninja',
+          9: 'Jigglypuff', 10: 'Lapras', 11: 'Machop', 12: 'Magikarp',
+          13: 'Meowth', 14: "MrMime", 15: 'Pikachu', 16: 'Rattata',
+          17: 'Rayquaza', 18: 'Snorlax', 19: 'Squirtle'}
 
 
 @app.route('/', methods=['GET', 'POST'])
 def subir_imagen():
+    print(request)
     if request.method == 'POST':
+        # Guardamos la imagen que han subido como imagen.jpg
         imagen = request.files['imagen']
-        # Guardamos la imagen en el directorio de imagenes
         imagen.save('./imagen.jpg')
+        # Cargamos la imagen para que el modelo prediga
         imagen = image.load_img('./imagen.jpg', target_size=(128, 128))
         imagen_array = image.img_to_array(imagen)
         imagen_array = np.expand_dims(imagen_array, axis=0)
+        # Predecimos con el modelo y devolvemos la prediccion
         prediccion = modelo.predict(imagen_array)
         prediccion = np.argmax(prediccion)
-        clases = {0: 'Bulbasaur', 1: 'Caterpie', 2: 'Charizard', 3: 'Charmander', 4: 'Dragonite',
-                  5: 'Eevee', 6: 'Gengar', 7: 'Geodude', 8: 'Greninja', 9: 'Jigglypuff', 10: 'Lapras',
-                  11: 'Machop', 12: 'Magikarp', 13: 'Meowth', 14: "MrMime", 15: 'Pikachu', 16: 'Rattata',
-                  17: 'Rayquaza', 18: 'Snorlax', 19: 'Squirtle'}
         pokemon_predicho = clases[prediccion]
 
         return redirect(url_for('stats_pokemon', pokemon_name=pokemon_predicho))
 
     if request.method == 'GET':
-        print(request)
         # Mostramos el formulario HTML de subida de imagen
         return render_template('main.html')
 
@@ -48,41 +55,18 @@ def subir_imagen():
 
 @app.route('/result/<pokemon_name>')
 def stats_pokemon(pokemon_name):
-    # Voy a usar wikidex porque megadexter me ha bloqueado la IP :(
-    # Cogemos de la página web
-    url = 'https://wikidex.net/wiki/' + pokemon_name
-    # url = 'https://megadexter.com/dex/pokemon/' + pokemon_name
-    pagina = requests.get(url)
-    soup = BeautifulSoup(pagina.content, 'html.parser')
     print('#' * 50)
     print('Pokemon\t\t', pokemon_name)
 
-    # TIPO y HABILIDADES
-    # El tipo y las habilidades están en la tabla con clase "datos"
-    tabla = soup.find('table', class_='datos')
-    # El tipo está en la fila con titulo "Tipos a los que pertenece"
-    # y aparece en el título de los elementos <a>, pero en el primer
-    # elemento <a> solo pone "Tipo"
-    tabla_tipo = tabla.find('tr', title='Tipos a los que pertenece')
-    tabla_tipo = tabla_tipo.findAll('a')[1:]
-    tipos = [a['title'] for a in tabla_tipo]
-    print('Tipo\t\t', tipos)
+    # WIKIDEX
+    # De wikidex vamos a sacar el las resistencias, debilidades e inmunidades
+    url = 'https://wikidex.net/wiki/' + pokemon_name
+    pagina = requests.get(url)
+    soup = BeautifulSoup(pagina.content, 'html.parser')
+    print('#' * 50)
+    print('Wikidex')
 
-    # Las habilidades están en la fila con titulo "Habilidades que puede conocer"
-    # y es una lista de elementos <a> con la habilidad en el texto
-    # igual saltandonos el primer elemento
-    # y las habilidades ocultas están en la fila con titulo "Habilidad oculta"
-    # y es un elemento <a> con la habilidad en el texto
-    tabla_habilidades = tabla.find('tr', title='Habilidades que puede conocer')
-    tabla_habilidades = tabla_habilidades.findAll('a')[1:]
-    tabla_ocultas = tabla.find('tr', title='Habilidad oculta')
-    tabla_ocultas = tabla_ocultas.findAll('a')[1:]
-    habilidades = [a.text for a in tabla_habilidades]
-    habilidades_ocultas = [a.text for a in tabla_ocultas]
-    habilidades.extend(habilidades_ocultas)
-    print('Habilidades\t', habilidades)
-
-    # DEBILIDADES y RESISTENCIAS
+    # DEBILIDADES
     # Estan en otra tabla con clase "tabpokemon", la ultima de la página
     tabla = soup.findAll('table', class_='tabpokemon')[-1]
     # print(tabla)
@@ -95,25 +79,62 @@ def stats_pokemon(pokemon_name):
     tabla_debilidades = [[a['title'] for a in d] for d in tabla_debilidades]
     debilidades = [w for d in tabla_debilidades for w in d]
     print('Debilidades\t', debilidades)
+
+    # RESISTENCIAS
     # Las resistencias están en las filas 4 y 5
-    # y la inmunidad en la fila 6
-    # En cada fila hay 3 elementos <td> y en el tercero está lo que queremos,
-    # una lista de <a> con la resistencia en el titulo
     tabla_resistencias = tabla.findAll('tr')[4:6]
     tabla_resistencias = [d.findAll('td')[2] for d in tabla_resistencias]
     tabla_resistencias = [d.findAll('a') for d in tabla_resistencias]
     tabla_resistencias = [[a['title'] for a in d] for d in tabla_resistencias]
     resistencias = [s for d in tabla_resistencias for s in d]
     print('resistencias\t', resistencias)
+
+    # INMUNIDADES
+    # Las inmunidades están en la fila 6
     tabla_inmunidad = tabla.findAll('tr')[6]
     tabla_inmunidad = tabla_inmunidad.findAll('td')[2]
     tabla_inmunidad = tabla_inmunidad.findAll('a')
     tabla_inmunidad = [a['title'] for a in tabla_inmunidad]
-    print('Inmunidades\t', tabla_inmunidad)
+    print('inmunidades\t', tabla_inmunidad)
 
-    # MOVIMIENTOS
-    # La información que queremos no está en wikidex, sino en megadexter
-    # pero ME HAN BLOQUEADO LA IP WTF
+    # MEGADEXTER
+    # De Megadexter vamos a sacar los tipos y habilidades
+    url = 'https://megadexter.com/dex/pokemon/' + pokemon_name
+    pagina = requests.get(url)
+    soup = BeautifulSoup(pagina.content, 'html.parser')
+    print('#' * 50)
+    print('Megadexter')
+    # Toda la información está en divs con clase "row"
+    tablas = soup.findAll('div', class_='row')
+
+    # TIPOS
+    # Los tipos están en la primera entrada
+    tabla_general = tablas[0].find('div', title=f'Tipos de {pokemon_name}')
+    tabla_general = tabla_general.findAll('a')
+    tipos = [a['title'] for a in tabla_general]
+    print('Tipos\t\t', tipos)
+
+    # HABILIDADES
+    # Las habilidades están en los divs con clase "panel-cristal"
+    tabla_habilidades = tablas[1].findAll('div', class_='panel-cristal')
+    # El nombre de la habildad está en el texto del elemento <a>
+    habilidades_nombre = [t.findAll('a') for t in tabla_habilidades]
+    habilidades_nombre = [a.text for t in habilidades_nombre for a in t]
+    # La descripción de la habilidad está en el texto de un elemento
+    # <div> con clase "panel-body"
+    habilidades_descripcion = [t.findAll('div', class_='panel-body') for t in tabla_habilidades]
+    habilidades_descripcion = [d.text for t in habilidades_descripcion for d in t]
+    # print(habilidades_nombre)
+    # print(habilidades_descripcion)
+    habilidades = dict(zip(habilidades_nombre, habilidades_descripcion))
+    print(habilidades)
+
+    # SMOGON
+    # De Smogon vamos a sacar los movimientos
+    url = 'https://www.smogon.com/dex/rb/pokemon/' + pokemon_name
+    print('#' * 50)
+    print('Smogon')
+    # MOVIMENTOS
     print('#' * 50)
     return render_template('result.html', pokemon_name=pokemon_name,
                            types=tipos, abilities=habilidades,
